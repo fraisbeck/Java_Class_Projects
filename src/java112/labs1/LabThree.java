2b8b769b09df806968066c8801b062a91efff020
Frank <fraisbeck@madisoncollege.edu>
2021-01-31 13:56:00 -0600

    Adding Unit 1 Lab 3

---
+++ src/java112/labs1/LabThree.java
@@ -0,0 +1,24 @@
+package java112.labs1;
+
+/** LabThree class continues practice with TDD
+ * @author fraisbeck
+ */
+
+public class LabThree {
+    /** main method to run our program
+     * @param args Command Line Arguments
+     */
+    public static void main(String[] args) {
+        if (args.length != 1) {
+            System.out.println("Please enter one argument on the command line");
+        } else {
+            LabThree lab = new LabThree();
+            lab.run(args[0]);
+        }
+    }
+    /** run
+     */
+    public void run (String myString) {
+        System.out.println("input: " + myString;
+    }
+}
